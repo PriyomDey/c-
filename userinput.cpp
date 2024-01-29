feb899ce9a89778b34db56f1a47f12a9f909add8
PriyomDey <157955824+PriyomDey@users.noreply.github.com>
2024-01-29 20:35:49 +0530

    Add files via upload

---
+++ userinput.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+string name;
+int age;
+char gender;
+float weight;
+
+cout<<"Enter your name: ";
+cin>>name;
+cout<<"Enter your age: ";
+cin>>age;
+cout<<"Mention your gender(M/F): ";
+cin>>gender;
+cout<<"Enter your weight: ";
+cin>>weight;
+
+cout<<"Your name is: "<<name<<endl;
+cout<<"Your age is: "<<age<<endl;
+cout<<"Your gender is: "<<gender<<endl;
+cout<<"Your weight is: "<<weight<<endl;
+
+
+
+    return 0;
+}
